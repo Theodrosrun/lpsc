92dae9dd8b021c1173969eabf4e58545c73ebddc
Theodros Mulugeta <theodros.mulugeta@runciter.ch>
2026-04-11 19:22:29 +0200

    fix: reset gitignore

--- scalp_revc/hw_firmware/soc/hw/scalp_zynqps_user/src/ipi_tcl/scalp_zynqps_user_ipi.tcl
+++
@@ -1,4 +0,0 @@
-set where [file dirname [info script]]
-#source [file join $where sys_clk_reset_system.tcl]
-source [file join $where vga_hdmi_clk_rst_system.tcl]
-source [file join $where platform.tcl]
