ffeaeca8abc923d86b01e83d6a8351664004b1f2
André Costa <andre_miguel_costa@hotmail.com>
2026-05-19 19:13:20 +0200

    fix: remove entity declaration of pipelined version of picture_gen

--- scalp_revc/hw_firmware/designs/vivado/scalp_user_design/2023.2/src/hdl/mandelbrot_picture_gen_pipelined.vhd
+++ scalp_revc/hw_firmware/designs/vivado/scalp_user_design/2023.2/src/hdl/mandelbrot_picture_gen_pipelined.vhd
@@ -3,31 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use std.textio.all;
 
-entity mandelbrot_picture_gen_pipelined is
-	generic (
-			C_BUFFER_WIDTH       : integer := 64;
-			C_BUFFER_HEIGHT      : integer := 64;
-			C_BRAM_ADDR_BIT_SIZE : integer := 12;
-			DATA_W               : integer := 18;
-			FRAC_W               : integer := 15;
-			MAX_ITER             : integer := 100
-		);
-	port (
-		     ClkxCI        : in  std_logic;
-		     RstxRANI      : in  std_logic;
-		     X0xDI         : in  signed(DATA_W-1 downto 0);
-		     Y0xDI         : in  signed(DATA_W-1 downto 0);
-		     DxxDI         : in  signed(DATA_W-1 downto 0);
-		     DyxDI         : in  signed(DATA_W-1 downto 0);
-		     StartxDI      : in  std_logic;
-		     BramWrAddrxDO : out std_logic_vector(C_BRAM_ADDR_BIT_SIZE-1 downto 0);
-		     BramWrDataxDO : out std_logic_vector(6 downto 0);
-		     BramWexSO     : out std_logic_vector(0 downto 0);
-		     FrameDonexSO  : out std_logic
-	     );
-end entity mandelbrot_picture_gen_pipelined;
-
-architecture rtl of mandelbrot_picture_gen_pipelined is
+architecture pipelined of mandelbrot_picture_gen is
 
 	constant NUM_SLOTS : integer := 3;
 
