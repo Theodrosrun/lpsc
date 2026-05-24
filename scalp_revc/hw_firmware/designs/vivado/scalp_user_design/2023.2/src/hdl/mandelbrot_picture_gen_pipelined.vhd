f626fb5afb1beb0fc11527686d3b975c0b0a2a79
Theodros Mulugeta <theodros.mulugeta@runciter.ch>
2026-05-24 22:08:58 +0200

    feat: update entity

--- scalp_revc/hw_firmware/designs/vivado/scalp_user_design/2023.2/src/hdl/mandelbrot_picture_gen_pipelined.vhd
+++ scalp_revc/hw_firmware/designs/vivado/scalp_user_design/2023.2/src/hdl/mandelbrot_picture_gen_pipelined.vhd
@@ -78,7 +78,7 @@ architecture pipelined of mandelbrot_picture_gen is
 
 begin
 
-	u_iter : entity work.mandelbrot_iter
+	u_iter : entity work.mandelbrot_iter(pipelined)
 	generic map (DATA_W => DATA_W, FRAC_W => FRAC_W)
 	port map (
 			 clk     => ClkxCI,
